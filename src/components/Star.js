3103c16024a3af8d7f12b453a410728a39a21c3b
theguywithnorace <47307776+theguywithnorace@users.noreply.github.com>
2019-06-07 20:50:46 +0200

    Update Star.js

--- src/components/Star.js
+++ src/components/Star.js
@@ -7,7 +7,7 @@ const STAR_SIZE = 40;
 
 export default class Star extends PureComponent {
   static defaultProps = {
-    selectedColor: '#181687'
+    selectedColor: '#141275'
   };
 
   constructor() {
